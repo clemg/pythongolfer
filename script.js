ec4c84ade79dd2a4fbbd641872bbe021407bab9c
Clément Guibout <clement.guibout@gmail.com>
2021-01-23 23:32:18 +0100

    Refactored script.js according to codacy standards

--- script.js
+++ script.js
@@ -1,60 +1,10 @@
 function init() {
-  output.value = "Ready!";
-}
-
-function golfcode() {
-  const code = document.getElementById("code");
-  const output = document.getElementById("output");
-
-  code.value = code.value.replace(/^\s*[\r\n]/gm, ""); // Blank lines
-  code.value = code.value.replace(/\s*$/gm, ""); // Trailling spaces at end of lines
-
-  try {
-    if (code.value.length % 2 != 0) {
-        code.value = code.value.concat(" ");
-    }
-
-    if (code.value.includes("+ ") || code.value.includes(". ")) {
-      if (document.getElementById("errorAlertDiv").childElementCount === 0) {
-        var errorAlert = document.createElement("p");
-        errorAlert.textContent = "Alert: Your code contains \"+ \" or \". \", and this causes a bug which reverses a part of the output. Consider removing this from your input code if the output does not work.";
-
-        document.getElementById("errorAlertDiv").appendChild(errorAlert);
-      }
-    } else {
-      var errorDiv = document.getElementById("errorAlertDiv");
-
-      errorDiv.querySelectorAll("*").forEach(n => n.remove());
-    }
-
-    var buffer = new ArrayBuffer(code.value.length);
-    var bufferView = new Uint16Array(buffer);
-
-    for (var i = 0, strLen = code.value.length; i < strLen - 1; i += 2) {
-      var c1 = code.value.charCodeAt(i);
-      var c2 = code.value.charCodeAt(i + 1);
-
-      if ((c1 || c2) > 127) {
-        throw("At least one of your code char is invalid.\nAll your chars in your code must be in the ASCII table.");
-      }
-
-      bufferView[i / 2] = c2 << 8 | c1;
-    }
-
-    output.value = "exec(bytes('" + String.fromCharCode.apply(String, bufferView) + "','u16')[2:])";
-    displayStats();
-  } catch (error) {
-    output.value = "An error occured. Refresh the page or check the logs.";
-    console.log("An error occured:\n" + error +
-      "\nPlease visit: https://github.com/clemg/pythongolfer/blob/main/README.md#qa" +
-      "\nMaybe the problem will be listed there. If not, you can open an issue."
-    );
-  }
+  document.getElementById("output").value = "Ready!";
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   document.body.addEventListener("keydown", function(event) {
-    if(!(event.keyCode == 13 && (event.metaKey || event.ctrlKey))) return;
+    if(!(event.keyCode === 13 && (event.metaKey || event.ctrlKey))) { return }
 
     golfcode();
   });
@@ -81,7 +31,7 @@ function calculateCharsCode() {
     codeCharsP.style.visibility = "visible";
   }
 
-  codeChars.innerHTML = charCount(code.value);
+  codeChars.innerHTML = charCount(document.getElementById("code").value);
 }
 
 function displayStats() {
@@ -101,4 +51,54 @@ function displayStats() {
   
   document.getElementById("stats").style.visibility = "visible";
   document.getElementById("copyButton").style.visibility = "visible";
+}
+
+function golfcode() {
+  const code = document.getElementById("code");
+  const output = document.getElementById("output");
+
+  code.value = code.value.replace(/^\s*[\r\n]/gm, ""); // Blank lines
+  code.value = code.value.replace(/\s*$/gm, ""); // Trailling spaces at end of lines
+
+  try {
+    if (code.value.length % 2 !== 0) {
+        code.value = code.value.concat(" ");
+    }
+
+    if (code.value.includes("+ ") || code.value.includes(". ")) {
+      if (document.getElementById("errorAlertDiv").childElementCount === 0) {
+        var errorAlert = document.createElement("p");
+        errorAlert.textContent = "Alert: Your code contains \"+ \" or \". \", and this causes a bug which reverses a part of the output. Consider removing this from your input code if the output does not work.";
+
+        document.getElementById("errorAlertDiv").appendChild(errorAlert);
+      }
+    } else {
+      var errorDiv = document.getElementById("errorAlertDiv");
+
+      errorDiv.querySelectorAll("*").forEach((n) => n.remove());
+    }
+
+    var buffer = new ArrayBuffer(code.value.length);
+    var bufferView = new Uint16Array(buffer);
+
+    for (var i = 0, strLen = code.value.length; i < strLen - 1; i += 2) {
+      var c1 = code.value.charCodeAt(i);
+      var c2 = code.value.charCodeAt(i + 1);
+
+      if ((c1 || c2) > 127) {
+        throw("At least one of your code char is invalid.\nAll your chars in your code must be in the ASCII table.");
+      }
+
+      bufferView[i / 2] = c2 << 8 | c1;
+    }
+
+    output.value = "exec(bytes('" + String.fromCharCode.apply(String, bufferView) + "','u16')[2:])";
+    displayStats();
+  } catch (error) {
+    output.value = "An error occured. Refresh the page or check the logs.";
+    console.log("An error occured:\n" + error +
+      "\nPlease visit: https://github.com/clemg/pythongolfer/blob/main/README.md#qa" +
+      "\nMaybe the problem will be listed there. If not, you can open an issue."
+    );
+  }
 }
